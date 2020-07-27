178dc61a4f9074cdb03bebaa7c4ba3bcc4270cf9
Glitch (hello-webpage) <none>
2020-07-27 08:48:48 +0000

    👐🚊 Checkpoint ./index.html:4783939/567 ./submission.html:4783939/809 ./script.js:4783939/387

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 /* Edit this javascript!*/
 
 
+      function lightMode(){
+        document.querySelector("body").style.backgroundColor = "#ccc";
+        document.querySelector("body").style.Color = "#000";
+      }
+
       function submitFunction() {
         alert("Your form has been submitted!");
-        document.querySelector("body").style.backgroundColor = "#ccc";
+        
+        window.location.href='/submission.html'
       }
 
   
